698684c00d97360a089f49feed9c415fc185e3ac
Josué Felipe <josueflx1999@gmail.com>
2020-12-15 21:38:10 -0300

    Created Project

---
+++ src/test/java/com/reign/animezation/AnimezationSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reign.animezation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimezationSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
