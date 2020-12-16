9aee6a2ba6b61bbb537404ce3dd39ece8e2ef442
Josué Felipe <josueflx1999@gmail.com>
2020-12-15 21:51:21 -0300

    --- User e UserResource ---
    	--- User.java ---
    * Criando entidade User
    * Definindo Atributos
    * Implementando metodo construtor
    * Implementando metodos Getters e Setters
    * Implementando metodos Equals e HashCode
    
    	--- UserResource.java ---
    * Criando classe
    * Implementando metodo findAll()
    * Adicionando anotações para o JPA

---
+++ src/main/java/com/reign/animezation/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.reign.animezation.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reign.animezation.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Teste", "teste@gmail.com", "999999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
