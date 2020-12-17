662c11a4c9cbe914ae41ab657f34a69159503d7a
Josué Felipe <josueflx1999@gmail.com>
2020-12-17 20:14:28 -0300

    --- Projeto criado novamente ---

---
+++ src/main/java/com/reign/animezation/AnimezationSbApplication.java
@@ -0,0 +1,13 @@
+package com.reign.animezation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimezationSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimezationSbApplication.class, args);
+	}
+
+}
