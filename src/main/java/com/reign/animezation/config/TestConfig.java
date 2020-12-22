498188f295063c11c8b12594c4b2fc4c9828ab27
Josué Felipe <josueflx1999@gmail.com>
2020-12-21 21:29:49 -0300

    --- OrderStatus enum ---
    	--- OrderStatus.java ---
    * Criando enum
    * Adicionando valores padrão setados manualmente
    * Adicionando atributo
    * Implementando metodo construtor
    * Implementando metodo getCode()
    * Implementando metodo valueOf()
    
    	--- Order.java ---
    * Adicionando atributo orderStatus
    * Alterando metodo construtor com parametros
    * Implementando metodo get e set de orderStatus
    
    	--- TestConfig.java ---
    * Alterando instancia de Order

--- src/main/java/com/reign/animezation/config/TestConfig.java
+++ src/main/java/com/reign/animezation/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.reign.animezation.entities.Order;
 import com.reign.animezation.entities.User;
+import com.reign.animezation.entities.enums.OrderStatus;
 import com.reign.animezation.repositories.OrderRepository;
 import com.reign.animezation.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Pablo Rodrigues", "pablorodrigo1996@hotmail.com", 11970707070L, "Gajuel123", null, null, null, null, null, null, null, null, null);
 		User u2 = new User(null, "Gabriel Tomaz", "gazinhogodi@outlook.com", 11960606060L, "Gabriel39234500gazinho", null, null, null, null, null, null, null, null, null);
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
