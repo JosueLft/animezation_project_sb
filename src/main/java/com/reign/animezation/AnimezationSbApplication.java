c0ef75a120b1715f715b4a5f4618020ace55918e
Josué Felipe <josueflx1999@gmail.com>
2020-12-31 19:45:00 -0300

    --- index, products ---
    	--- index.html ---
    * Alteração do menu superior
    * Remoção do banner de imagens
    * Adição de painel com logo

--- src/main/java/com/reign/animezation/AnimezationSbApplication.java
+++ src/main/java/com/reign/animezation/AnimezationSbApplication.java
@@ -2,8 +2,12 @@ package com.reign.animezation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.reign.animezation.entities"})
+@EnableJpaRepositories(basePackages = {"com.reign.animezation.repositories"})
 public class AnimezationSbApplication {
 
 	public static void main(String[] args) {
